e538e1719a8674767e13eed949c20950ee953298
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-03-25 22:35:27 -0300

    :recycle: rename var

--- src/test/java/domain/UNQFlixBannerTest.kt
+++ src/test/java/domain/UNQFlixBannerTest.kt
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Assertions.*
 class UNQFlixBannerTest {
     @Test
     fun addBanner() {
-        val unQflix = UNQFlix()
-        assertEquals(unQflix.series.size, 0)
+        val unqflix = UNQFlix()
+        assertEquals(unqflix.series.size, 0)
 
         val serie1 = SerieBuilder.with(id = "ser_1", title = "ser1")
         val serie2 = SerieBuilder.with(id = "ser_2", title = "ser2")
@@ -16,36 +16,36 @@ class UNQFlixBannerTest {
         val serie5 = SerieBuilder.with(id = "ser_5", title = "ser5")
         val serie6 = SerieBuilder.with(id = "ser_6", title = "ser6")
 
-        unQflix.addBanner(serie1)
-        unQflix.addBanner(serie2)
-        unQflix.addBanner(serie3)
-        unQflix.addBanner(serie4)
-        unQflix.addBanner(serie5)
+        unqflix.addBanner(serie1)
+        unqflix.addBanner(serie2)
+        unqflix.addBanner(serie3)
+        unqflix.addBanner(serie4)
+        unqflix.addBanner(serie5)
 
-        assertEquals(unQflix.banners.size, 5)
+        assertEquals(unqflix.banners.size, 5)
 
-        unQflix.addBanner(serie6)
-        assertEquals(unQflix.banners.size, 5)
-        assertFalse(unQflix.banners.contains(serie1))
+        unqflix.addBanner(serie6)
+        assertEquals(unqflix.banners.size, 5)
+        assertFalse(unqflix.banners.contains(serie1))
     }
 
     @Test
     fun deleteBanner() {
-        val unQflix = UNQFlix()
-        assertEquals(unQflix.series.size, 0)
+        val unqflix = UNQFlix()
+        assertEquals(unqflix.series.size, 0)
 
         val serie1 = SerieBuilder.with(id = "ser_1", title = "ser1")
         val serie2 = SerieBuilder.with(id = "ser_2", title = "ser2")
 
-        unQflix.addBanner(serie1)
-        unQflix.addBanner(serie2)
+        unqflix.addBanner(serie1)
+        unqflix.addBanner(serie2)
 
-        assertEquals(unQflix.banners.size, 2)
+        assertEquals(unqflix.banners.size, 2)
 
-        unQflix.deleteBanner(serie2)
+        unqflix.deleteBanner(serie2)
 
-        assertEquals(unQflix.banners.size, 1)
-        assertTrue(unQflix.banners.contains(serie1))
-        assertFalse(unQflix.banners.contains(serie2))
+        assertEquals(unqflix.banners.size, 1)
+        assertTrue(unqflix.banners.contains(serie1))
+        assertFalse(unqflix.banners.contains(serie2))
     }
 }
--- src/test/java/domain/UNQFlixUserBehaviorTest.kt
+++ src/test/java/domain/UNQFlixUserBehaviorTest.kt
@@ -6,67 +6,67 @@ import org.junit.jupiter.api.Assertions.*
 class UNQFlixUserBehaviorTest {
     @Test
     fun addLastSeen() {
-        val unQflix = UNQFlix()
-        assertEquals(unQflix.users.size, 0)
-        assertEquals(unQflix.series.size, 0)
+        val unqflix = UNQFlix()
+        assertEquals(unqflix.users.size, 0)
+        assertEquals(unqflix.series.size, 0)
 
         val user = UserBuilder.any()
-        unQflix.addUser(user)
+        unqflix.addUser(user)
 
-        assertEquals(unQflix.users.size, 1)
+        assertEquals(unqflix.users.size, 1)
         assertEquals(user.lastSeen.size, 0)
 
         val serie = SerieBuilder.with(id = "ser_1")
-        unQflix.addSerie(serie)
-        assertEquals(unQflix.series.size, 1)
+        unqflix.addSerie(serie)
+        assertEquals(unqflix.series.size, 1)
 
-        unQflix.addLastSeen("1", "ser_1")
+        unqflix.addLastSeen("1", "ser_1")
         assertEquals(user.lastSeen.size, 1)
     }
 
     @Test
     fun addLastSeenTwoTimes() {
-        val unQflix = UNQFlix()
-        assertEquals(unQflix.users.size, 0)
-        assertEquals(unQflix.series.size, 0)
+        val unqflix = UNQFlix()
+        assertEquals(unqflix.users.size, 0)
+        assertEquals(unqflix.series.size, 0)
 
         val user = UserBuilder.any()
-        unQflix.addUser(user)
+        unqflix.addUser(user)
 
-        assertEquals(unQflix.users.size, 1)
+        assertEquals(unqflix.users.size, 1)
         assertEquals(user.lastSeen.size, 0)
 
         val serie = SerieBuilder.with(id = "ser_1")
-        unQflix.addSerie(serie)
-        assertEquals(unQflix.series.size, 1)
+        unqflix.addSerie(serie)
+        assertEquals(unqflix.series.size, 1)
 
-        unQflix.addLastSeen("1", "ser_1")
+        unqflix.addLastSeen("1", "ser_1")
         assertEquals(user.lastSeen.size, 1)
 
-        unQflix.addLastSeen("1", "ser_1")
+        unqflix.addLastSeen("1", "ser_1")
         assertEquals(user.lastSeen.size, 1)
     }
 
     @Test
     fun addOrDeleteFav() {
-        val unQflix = UNQFlix()
-        assertEquals(unQflix.users.size, 0)
-        assertEquals(unQflix.series.size, 0)
+        val unqflix = UNQFlix()
+        assertEquals(unqflix.users.size, 0)
+        assertEquals(unqflix.series.size, 0)
 
         val user = UserBuilder.any()
-        unQflix.addUser(user)
+        unqflix.addUser(user)
 
-        assertEquals(unQflix.users.size, 1)
+        assertEquals(unqflix.users.size, 1)
         assertEquals(user.favorites.size, 0)
 
         val serie = SerieBuilder.with(id = "ser_1")
-        unQflix.addSerie(serie)
-        assertEquals(unQflix.series.size, 1)
+        unqflix.addSerie(serie)
+        assertEquals(unqflix.series.size, 1)
 
-        unQflix.addOrDeleteFav("1", "ser_1")
+        unqflix.addOrDeleteFav("1", "ser_1")
         assertEquals(user.favorites.size, 1)
 
-        unQflix.addOrDeleteFav("1", "ser_1")
+        unqflix.addOrDeleteFav("1", "ser_1")
         assertEquals(user.favorites.size, 0)
     }
 }
